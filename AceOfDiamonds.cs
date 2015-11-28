fb6c29defc3b8c5177cb07c2d6ab2b4aad1cfc1f
VanGog06 <vangog06406@gmail.com>
2015-11-28 19:25:29 +0200

    Update AceOfDiamonds.cs

--- AceOfDiamonds.cs
+++ AceOfDiamonds.cs
@@ -1,3 +1,42 @@
+/*  Twisted Fate loves to play with cards! His favourite one is Ace of Diamonds. Your task is to draw Ace of Diamonds on the conole by given width/height. See examples below to understand the task better.
+
+Input
+The input data should be read from the console. It consists of one input value:
+•	On the only input line you will recieve width/height of the card.
+The input data will always be valid and in the format described. There is no need to check it explicitly.
+
+Output
+The Ace of Diamonds must be printed on the console. 
+
+Constraints
+•	The number will be odd integer number in range [3... 4,294,967,295].
+•	Allowed working time for your program: 0.1 seconds.
+•	Allowed memory: 16 MB.
+
+Examples
+		
+Input	Output	                             	
+3    	***
+        *@*
+        ***		
+        
+Input   Output     
+5	    *****
+        *-@-*
+        *@@@*
+        *-@-*
+        *****
+        
+Input   Output        
+7	    *******
+        *--@--*
+        *-@@@-*
+        *@@@@@*
+        *-@@@-*
+        *--@--*
+        *******
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
